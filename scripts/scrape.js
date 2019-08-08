cff958b03ac859ff1e532aac16d6110575815d1e
Andrew Hok <ahok89@gmail.com>
2019-08-08 12:40:53 -0400

    revised scraper

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,49 +1,58 @@
+// scrape script
+// =============
 
+// Require axios and cheerio, making our scrapes possible
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-
+// This function will scrape the NYTimes website
 var scrape = function() {
-
-  return axios.get("https://www.npr.org").then(function(res) {
+  // Scrape the NYTimes website
+  return axios.get("https://www.npr.org/sections/world").then(function(res) {
     var $ = cheerio.load(res.data);
     console.log("scraping");
-
+    // Make an empty array to save our article info
     var articles = [];
 
+    // Now, find and loop through each element that has the ".assetWrapper" class
+    // (i.e, the section holding the articles)
+    $("article h2").each(function(i, element) {
+      // In each article section, we grab the headline, URL, and summary
 
-    $(".assetWrapper").each(function(i, element) {
-
+      // Grab the headline of the article
       var head = $(this)
-        .find("h2")
+        .children("a")
         .text()
         .trim();
 
-
+      // Grab the URL of the article
       var url = $(this)
         .find("a")
         .attr("href");
 
-
+      // Grab the summary of the article
       var sum = $(this)
-        .find("p")
+        .parent("")
+        .children("p")
+        .children("a")
         .text()
         .trim();
 
-
+      // So long as our headline and sum and url aren't empty or undefined, do the following
       if (head && sum && url) {
-
+        // This section uses regular expressions and the trim function to tidy our headlines and summaries
+        // We're removing extra lines, extra spacing, extra tabs, etc.. to increase to typographical cleanliness.
         var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
         var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
 
-
+        // Initialize an object we will push to the articles array
         var dataToAdd = {
           headline: headNeat,
           summary: sumNeat,
-          url: "https://www.npr.org" + url
+          url: "https://www.npr.org/sections/world" + url
         };
 
-
+        // Push new article into articles array
         articles.push(dataToAdd);
       }
     });
@@ -51,5 +60,5 @@ var scrape = function() {
   });
 };
 
-
+// Export the function, so other files in our backend can use it
 module.exports = scrape;
